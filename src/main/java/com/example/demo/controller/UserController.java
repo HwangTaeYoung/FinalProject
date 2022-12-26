711788c987c8c892e825f84cec9b38eae27fee80
hwang tae young <tye1990@naver.com>
2022-12-26 12:22:11 +0900

    로그인페이지 html 및 css 작업물입니다.

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.service.UserService;
+
+@Controller
+public class UserController {
+	// 서비스선언
+	@Resource(name="com.example.demo.service.UserService")
+	UserService muserService;
+	
+	@RequestMapping(value = "/login") // url, 매핑 시킬 페이지 값(form태그 action에 설정된 값)을 설정
+	public String LoginController() { // 실제 사용 할 메소드 생성
+
+		return "/main/user/login"; // 이동파일(jsp)을 사용하는 구간
+	}
+}
--- src/main/webapp/WEB-INF/views/main/include/head.jsp
+++ src/main/webapp/WEB-INF/views/main/include/head.jsp
@@ -2,6 +2,7 @@
     pageEncoding="UTF-8"%>
 <link rel="stylesheet" href="/css/seondoo.css">
 <link rel="stylesheet" href="/css/common.css">
+<link rel="stylesheet" href="/css/taeyoung.css">
 <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/xeicon@2.3.3/xeicon.min.css">
 <link rel="stylesheet" href="https://unpkg.com/aos@2.3.1/dist/aos.css"> 
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script> 
